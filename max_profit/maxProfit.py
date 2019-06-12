2c61796a490548b6ccc6d8f69a2f264270da7bf6
Devavrat <devavratk96@gmail.com>
2019-06-12 19:05:21 -0400

    Two codes added

---
+++ max_profit/maxProfit.py
@@ -0,0 +1,49 @@
+"""
+    File name: maxProfit.py
+    Authod: Devavrat Kalam
+    Language: Python 3.x
+    Description: Compute maximum profit from a single valid transaction(buy first and sell later) from given array
+    Time Complexity: O(n)
+"""
+
+
+def maxProfit(arr):
+    """
+    Compute the maximum profit
+    :param arr: arr of integers
+    :return: Maximum profit
+    """
+
+    if not arr:
+        return 0
+
+    # Initializing min and max vals to extreme
+    minVal = max(arr)
+    maxVal = 0
+
+    for elem in arr:
+        if elem < minVal:
+            # if current value is less than current minimumVal, replace it
+            minVal = elem
+        elif (elem - minVal) > maxVal:
+            # If profit is more than current profit, replace it
+            maxVal = elem - minVal
+
+    return maxVal
+
+
+def main():
+    # Test cases:
+    time_intervals = [7, 2, 5, 4, 3, 10]
+    # time_intervals = [1, 1, 1]
+    # time_intervals = [7, 6, 5, 4, 3, 2, 1]
+    # time_intervals = [7, 8, 9, 10]
+    # time_intervals = [10]
+    # time_intervals = []
+
+    print(maxProfit(time_intervals))
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ time_interval/time_interval.py
@@ -0,0 +1,59 @@
+"""
+    File name: time_interval.py
+    Authod: Devavrat Kalam
+    Language: Python 3.x
+    Description: Given pairs time intervals (start, end), determine minimum number of classroom required
+    Time Complexity: O(n log(n))
+"""
+
+
+def time_interval(intervals):
+    """
+    Find minimum number of rooms need
+    :param intervals: list of pairs of start and end times
+    :return: minimum rooms required
+    """
+
+    start = []
+    finish = []
+    for elems in intervals:
+        start.append(elems[0])
+        finish.append(elems[1])
+
+    # Sorting the start and end times separately
+    start.sort()
+    finish.sort()
+
+    index1, index2 = 0, 0
+    current_rooms = 0
+    max_rooms = 0
+    # The logic below is, we add each room when current finish time is greater than current start time
+    # This informs us whether how many times are collapsing with each other at any time
+    while (index1 < len(start)) and (index2 < len(finish)):
+        if start[index1] < finish[index2]:
+            current_rooms += 1
+            index1 += 1
+        else:
+            index2 += 1
+            max_rooms = max(max_rooms, current_rooms)
+            current_rooms -= 1
+    max_rooms = max(max_rooms, current_rooms)
+    return max_rooms
+
+def main():
+    # Test cases:
+    intervals = [
+        (30, 75),
+        (0, 50),
+        (60, 150)
+    ]
+    # intervals = [(0, 10),(11, 20),(21, 30)]
+    # intervals = [(0, 10),(10, 20),(20, 30)]
+    # intervals = [(2, 30),(1, 20),(0, 10)]
+    # intervals = [(1, 2)]
+    # intervals = []
+
+    print(time_interval(intervals))
+
+if __name__ == '__main__':
+    main()
